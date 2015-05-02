b874e36b75e232246b43cf11a12f3d61cb08d89c
milen-vm <bay_milen@abv.bg>
2015-05-02 18:25:17 +0300

    Starting the project

---
+++ includes/config.php
@@ -0,0 +1,11 @@
+<?php
+define('DEFAULT_CONTROLLER', 'home');
+define('DEFAULT_ACTION', 'index');
+define('DEFAULT_LAYOUT', 'default');
+
+define('SERVER_NAME', 'localhost');
+define('USER_NAME', 'root');
+define('SERVER_PASSWORD', '');
+define('DATABASE_NAME', 'photo_album');
+
+define('USER_NAME_MIN_LENGTH', 3);
